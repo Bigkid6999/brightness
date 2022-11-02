4545550c2788557e431b11692e27f594eff89cb2
Bigkid6999 <112982143+Bigkid6999@users.noreply.github.com>
2022-11-02 13:41:56 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-console.log("hi");
+// BRIGHTNESS IPO CALC BY MR.F
+
+//Input
+let R = +prompt("Enter a R color value (Red):");
+let G = +prompt("Enter a G color value (Green):");
+let B = +prompt("Enter a B color value(blue):");
+
+console.log(R, G, B);
+
+//Process
+
+//Output
