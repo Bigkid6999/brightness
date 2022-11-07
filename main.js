c9cc01f9ecb0ea47af15a84bb1f8f0ca35bd91e7
Bigkid6999 <112982143+Bigkid6999@users.noreply.github.com>
2022-11-07 13:38:04 -0700

    Update main.js

--- main.js
+++ main.js
@@ -5,7 +5,6 @@ let R = +prompt("Enter a R color value (Red):");
 let G = +prompt("Enter a G color value (Green):");
 let B = +prompt("Enter a B color value(blue):");
 
-console.log(R, G, B);
 
 //Process
 
