2b5da1d738b7a95fe151e99836e9cc5490160a28
Bigkid6999 <112982143+Bigkid6999@users.noreply.github.com>
2022-12-12 13:49:13 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
 // BRIGHTNESS IPO CALC BY MR.F
 
+// Event listener on the button
+document.getElementById('btn').addEventListiner('click', btnClicked);
+
+function btnClicked() {
+    
+}
+
 //Input
 let R = +prompt("Enter a R color value (Red):");
 let G = +prompt("Enter a G color value (Green):");
@@ -10,4 +17,4 @@ let B = +prompt("Enter a B color value(blue):");
 let brightness = Math.sqrt(0.299(R) ** 2 + 0.587(G) ** 2 + 0.114(B) ** 2);  
 
 //Output
-alert ('$(brightness) is the brightness');
+alert ('the brightness is:${brightness}');
